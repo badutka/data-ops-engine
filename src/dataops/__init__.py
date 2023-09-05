e4b917bad93e95b4f6024ba88487656862d9af80
Bartosz Dutka <dutkabar@gmail.com>
2023-09-05 19:10:18 +0200

    added docstrings

--- src/dataops/__init__.py
+++ src/dataops/__init__.py
@@ -11,27 +11,56 @@ __version__ = "0.0.1"
 
 
 def load_settings():
-    # todo: https://stackoverflow.com/questions/6198372/most-pythonic-way-to-provide-global-configuration-variables-in-config-py
+    """
+    Read root:settings.yaml file into a dictionary
+    https://stackoverflow.com/questions/6198372/most-pythonic-way-to-provide-global-configuration-variables-in-config-py
+
+    Returns:
+        dict: settings loaded from yaml file
+    """
     current_file_path = os.path.abspath(__file__)
     file_path = os.path.join(os.path.dirname(current_file_path), "settings.yaml")
+
     with open(file_path, "r") as file:
         settings = yaml.safe_load(file)
+
     return settings
 
 
 class MulticlassSettings(BaseModel):
+    """
+    Pydantic class to store and validate settings for multi-classification models
+    """
     file_name: str
     file_delimiter: typing.Union[str, None]
     max_nunique_for_column: typing.Union[int, None]
 
 
 class Settings(BaseModel):
+    """
+        Pydantic class to gather all lower level settings into a single object
+    """
     multiclass: MulticlassSettings
 
 
-def setup_logger():
+settings_data = load_settings()
+settings = Settings(**settings_data)
+
+
+def setup_logger(logger_name="dataops-logger", file_name="logs.log", loger_level='DEBUG'):
+    """
+    Create a logger object with two handlers - console and file.
+
+    Args:
+        logger_name (str): name of the logger object used to retrieve throughout the library (only one instance per name is created)
+        file_name (str): name of the file messages are logged into
+        loger_level (str): level at which logger starts to acknowledge messages
+
+    Returns (None): None
+
+    """
     formatter = logging.Formatter("%(message)s")
-    logger = logging.getLogger("dataops-logger")
+    logger = logging.getLogger(logger_name)
 
     logger.setLevel(logging.DEBUG)
 
@@ -40,14 +69,14 @@ def setup_logger():
     ch.setFormatter(formatter)
     logger.addHandler(ch)
 
-    fh = logging.FileHandler("logs.log")
+    fh = logging.FileHandler(file_name)
     fh.setLevel(logging.DEBUG)
     formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
     fh.setFormatter(formatter)
     logger.addHandler(fh)
 
     coloredlogs.install(
-        level='DEBUG',
+        level=loger_level,
         logger=logger,
         level_styles={
             'debug': {'color': 'blue'},
@@ -59,5 +88,3 @@ def setup_logger():
 
 
 setup_logger()
-settings_data = load_settings()
-settings = Settings(**settings_data)
